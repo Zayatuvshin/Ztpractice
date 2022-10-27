8fccf3b4ff02742a43663cb4de3d8e88d3265738
Zayatuvshin <zayatuvshin@letumongolia.com>
2022-10-27 20:20:52 +0800

    Create bmi.py

---
+++ bmi.py
@@ -0,0 +1,19 @@
+import streamlit as st
+from joblib import load
+st.title('BMI Calculator')
+st.header("Hello, let's calculate your body mass index")
+
+weight = st.number_input('weight', )
+height = st.number_input('height', )
+if st.button("Calculate BMI") == True:
+    (weight / (height ** 2)) * 10000
+if st.button('Body mass status') == True:
+    bmi = ((weight / (height ** 2)) * 10000)
+    if bmi <= 18.4:
+        st.write("You are underweight")
+    elif bmi > 18.5 and bmi < 24.9:
+        st.write("Your weight is normal")
+    elif ((weight / (height ** 2)) * 10000) > 25 and bmi < 39.9:
+        st.write("You are overweight")
+    elif ((weight / (height ** 2)) * 10000) >= 40:
+        st.write("You are obese")
